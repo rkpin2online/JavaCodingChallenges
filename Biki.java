2cb278c11b85ff7166e1c703d863b2ec17042016
rkpin2online <rkpin2online@gmail.com>
2020-01-16 18:22:01 +0530

    new file added

---
+++ Biki.java
@@ -0,0 +1,10 @@
+public Class Biki{
+	public static void main(String[] args){
+		System.out.println("Hi to Biki");
+		Biki b= new Biki();
+		b.call();
+	}
+	private void call(){
+		System.out.println("From Call Method");
+	}
+}
